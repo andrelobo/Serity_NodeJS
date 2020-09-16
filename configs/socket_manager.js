456fa1f3234181739d13716eb946eae336692eac
Juliano Lira <juliano0forum@gmail.com>
2020-09-16 13:28:19 -0300

    Projeto iniciado

--- configs/socket_manager.js
+++ configs/socket_manager.js
@@ -1,8 +1,15 @@
 
 const socket_manager = (io) => {
-    io.on("connection", function (client) {
+
+    io.on("connection", function (socket) {
         console.log("[!] New Connection")
+
+        socket.on("disconnect", function () {
+            console.log("[!] Closed Connection");
+         });
+
     });
+    
 }
 
 module.exports = socket_manager
