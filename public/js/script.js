add3fdbfc4dbedc4a7e8dc8a95c45a5ee732f3a4
ShintaroBRL <juliano0forum@gmail.com>
2020-09-22 13:21:41 -0300

    Novo template

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-﻿
+$('.sp-dropdown').click(function(){
+  			$(this).children('i').stop().toggleClass('fa-arrow-circle-up');
+  $(this).stop().toggleClass('sp-dropdown-active');
+  $(this).children('.sp-dropdown-menu').stop().animate({height: "toggle", 'padding-top': "toggle", 'padding-bottom': "toggle", opacity: "toggle"}, "slow");
+});
+
+$('.toggler').click(function() {
+  $('body').stop().toggleClass('sp-toggle-sidebar', 300);
+});
