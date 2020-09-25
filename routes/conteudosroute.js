ad593c506ac89db8dd8309316f7b63a1dc664d52
ShintaroBRL <juliano0forum@gmail.com>
2020-09-25 10:54:49 -0300

    Multer included, fixed conteudos front-end

--- routes/conteudosroute.js
+++ routes/conteudosroute.js
@@ -1,8 +1,15 @@
 
-const conteudosroute = (app,postgress_manager) => {
+const conteudosroute = (app,postgress_manager,uploader) => {
+
     app.get('/conteudos', (req, res) => {
        var metadata = postgress_manager.conteudo_select(req,res)
        res.render('conteudos', metadata)
+    })
+
+    app.post('/conteudos', uploader.array('anexos'), function (req, res, next) {
+        console.log(req.files)
+        console.log(req.body)
+        res.redirect('/conteudos')
     })
 
 }
--- start_server.js
+++ start_server.js
@@ -5,13 +5,13 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
 //custom requires
 const postgress_manager = require('./configs/postgress_manager')                    //Postegress Conector
 const socket_manager = require('./configs/socket_manager')(io, postgress_manager)   //Socket.io Server
+const multer_manager = require('./configs/multer_manager')                          //Multer uploader
 
 //MiddleWares
 app.set('view engine', 'ejs')
@@ -25,7 +25,7 @@ app.use(session({secret: "123123123123", resave: true, saveUninitialized: true})
 //Routes
 const indexroute = require('./routes/indexroute')(app,postgress_manager)
 const homeroute = require('./routes/homeroute')(app,postgress_manager)
-const conteudosroute = require('./routes/conteudosroute')(app,postgress_manager)
+const conteudosroute = require('./routes/conteudosroute')(app,postgress_manager,multer_manager)
 const trabalhosroute = require('./routes/trabalhosroute')(app,postgress_manager)
 const chatsroute = require('./routes/chatsroute')(app,postgress_manager)
 const avaliacoesroute = require('./routes/avaliacoesroute')(app,postgress_manager)
